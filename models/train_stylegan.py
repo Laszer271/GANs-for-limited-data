a9a3b1c67b51bb4aa48dc7295a191a407dfca37a
Lasze <Laszer2710@gmail.com>
2022-04-21 21:19:39 +0200

    deleting multiprocessing

--- models/train_stylegan.py
+++ models/train_stylegan.py
@@ -37,8 +37,8 @@ def subprocess_fn(rank, args, temp_dir):
             torch.distributed.init_process_group(backend='nccl', init_method=init_method, rank=rank, world_size=args.num_gpus)
 
     # Init torch_utils.
-    sync_device = torch.device('cuda', rank) if args.num_gpus > 1 else None
-    training_stats.init_multiprocessing(rank=rank, sync_device=sync_device)
+    #sync_device = torch.device('cuda', rank) if args.num_gpus > 1 else None
+    #training_stats.init_multiprocessing(rank=rank, sync_device=sync_device)
     if rank != 0:
         custom_ops.verbosity = 'none'
 
